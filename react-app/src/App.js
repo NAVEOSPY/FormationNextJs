0eb7756878c933f070de70a09ced0e2f15f624b3
Pierre-Yves NAVEOS <P.NAVEOS@beneteau-group.com>
2025-02-20 15:24:45 +0100

    My first pokemon

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,25 @@
 import "./intro.css";
 
 function App() {
-    return (
-        <div className="intro">
-            <div>Bienvenue sur ton futur pokédex !</div>
-            <div>
-                Tu vas pouvoir apprendre tout ce qu'il faut sur React, et
-                attraper des pokemons !
-            </div>
-            <div>Commence par créer ton premier pokemon: Mew !</div>
+  return (
+    <>
+      <div className="intro">
+        <div>Bienvenue sur ton futur pokédex !</div>
+        <div>
+          Tu vas pouvoir apprendre tout ce qu'il faut sur React, et attraper des
+          pokemons !
         </div>
-    );
+        <div>
+          Commence par créer ton premier pokemon:
+          <div className="justify-center">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/151.png" />
+            <p>Mew</p>
+            <p>151</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
