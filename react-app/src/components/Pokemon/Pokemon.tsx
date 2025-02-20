d0f819d049c3dad36a7fa5ace2d61b7311ff7c3a
--get-all <valentin.chion@ext.theodo.com>
2025-02-20 17:07:20 +0100

    Corrected based on review

--- react-app/src/components/Pokemon/Pokemon.tsx
+++ react-app/src/components/Pokemon/Pokemon.tsx
@@ -1,21 +1,21 @@
 interface Props {
-  name: string;
-  num: number;
+    name: string;
+    num: number;
 }
 
 const Pokemon = ({ name, num }: Props) => {
-  return (
-    <div>
-      <div className="justify-center">
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${num}.png`}
-          alt="pokemon"
-        />
-        <p>{name}</p>
-        <p>{num}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${num}.png`}
+                alt="pokemon"
+            />
+            <div>
+                <p>Nom: {name}</p>
+                <p>Numéro: {num}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Pokemon;
