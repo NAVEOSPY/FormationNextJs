b5a14108daeb807080d0282166731f41af84cfcd
Pierre-Yves NAVEOS <P.NAVEOS@beneteau-group.com>
2025-02-21 15:52:14 +0100

    filtrer mieux les pokemons

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,57 +2,55 @@ import "./intro.css";
 import React from "react";
 import Pokemon from "./components/Pokemon/Pokemon.tsx";
 
-function App() {
-    const [filterValue, setFilterValue] = React.useState("");
+const POKEMON_LIST: Pokemon[] = [
+  { name: "Mew", num: 151 },
+  { name: "Mewtwo", num: 150 },
+  { name: "Dracolosse", num: 149 },
+];
 
-    function filterPokemonsByName(
-        listePokemons: Pokemon[],
-        namePokemon: string,
-    ) {
-        const resultat = listePokemons.filter(
-            (poke) => poke.name === namePokemon,
-        );
-        console.log(resultat);
+export default function App() {
+  const [filterValue, setFilterValue] = React.useState("");
+  const [pokemonsValue, setpokemonsValue] = React.useState(POKEMON_LIST);
 
-        //return namePokemon;
-        //return listePokemons.find((resultat) => resultat.name === namePokemon);
-    }
-
-    const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFilterValue(event.target.value);
-        const listePokemonaFiltrer = [
-            { name: "M", num: 150 },
-            { name: "C", num: 7 },
-            { name: "S", num: 4 },
-        ];
-        console.log(
-            filterPokemonsByName(listePokemonaFiltrer, event.target.value),
-        );
-    };
-
-    return (
-        <>
-            <div className="intro">
-                <div>Bienvenue sur ton futur pokédex !</div>
-                <div>
-                    Tu vas pouvoir apprendre tout ce qu'il faut sur React, et
-                    attraper des pokemons !
-                </div>
-                <div>
-                    <input onChange={onInputChange} value={filterValue} />
-                </div>
-
-                <div>
-                    Commence par créer ton premier pokemon:
-                    <div>
-                        <Pokemon name="Mew" num={150} />
-                        <Pokemon name="Carapuce" num={7} />
-                        <Pokemon name="Salamèche" num={4} />
-                    </div>
-                </div>
-            </div>
-        </>
+  function filterPokemonsByName(listePokemons: Pokemon[], namePokemon: string) {
+    setpokemonsValue(
+      listePokemons.filter((poke) =>
+        poke.name.toLowerCase().startsWith(namePokemon.toLowerCase())
+      )
     );
-}
+  }
 
-export default App;
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterValue(event.target.value);
+    filterPokemonsByName(POKEMON_LIST, event.target.value);
+    //console.log(filterPokemonsByName(POKEMON_LIST, event.target.value));
+  };
+
+  return (
+    <>
+      <div className="intro">
+        <div>Bienvenue sur ton futur pokédex !</div>
+        <div>
+          Tu vas pouvoir apprendre tout ce qu'il faut sur React, et attraper des
+          pokemons !
+        </div>
+        <div>
+          <input onChange={onInputChange} value={filterValue} />
+        </div>
+
+        <div>
+          Commence par créer ton premier pokemon:
+          <div>
+            <ul>
+              {pokemonsValue.map((pok) => (
+                <li>
+                  <Pokemon name={pok.name} num={pok.num} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
