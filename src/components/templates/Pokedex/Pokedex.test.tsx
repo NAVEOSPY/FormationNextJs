6a8f8b49d1a7a56aa1f47159d5a4494b2e32d260
Pierre-Yves NAVEOS <P.NAVEOS@beneteau-group.com>
2025-03-05 15:56:35 +0100

    test pokedex

---
+++ src/components/templates/Pokedex/Pokedex.test.tsx
@@ -0,0 +1,38 @@
+import { expect, test, vi, Mock, describe } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import Pokedex from "./pokedex";
+
+const MOCK_POKEDEX_DATA = {
+  pokemon_entries: [
+    {
+      entry_number: 1,
+      pokemon_species: { name: "squirtle" },
+    },
+  ],
+};
+
+describe("<Pokedex />", () => {
+  test("Pokemons are displayed in Pokedex", async () => {
+    /*
+    vi.spyOn(global, "fetch").mockImplementation(
+      vi.fn(() =>
+        Promise.resolve({
+          json: () => Promise.resolve(MOCK_POKEDEX_DATA),
+        })
+      ) as Mock
+    );
+    */
+    const pokemonListTest = [
+      { entry_number: 1, pokemon_species: { name: "squirtle" } },
+    ];
+
+    render(<Pokedex pokemonsList={pokemonListTest} />);
+    await waitFor(() => {
+      expect(
+        screen.getByText("squirtle", {
+          exact: false,
+        })
+      ).toBeDefined();
+    });
+  });
+});
